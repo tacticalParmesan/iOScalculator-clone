8110774e66c7ff22520b0c9b2a0efb3d4696e3fb
tacticalParmesan <edoardoferreri10@gmail.com>
2024-02-05 19:28:37 +0100

    Implemented the operate function, to call all the other functions according to the input

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Basic arithmetic operations
-
 function add(firstAddend, secondAddend) {
 	return firstAddend + secondAddend;
 }
@@ -20,3 +19,24 @@ function divide(firstDividend, secondDividend) {
 		alert("Cannot divide by zero!");
 	}
 }
+
+// Expression evaluation
+function operate(operator, firstNumber, secondNumber) {
+	/* The operate function will call a different operation function according to the
+	expression given as input */
+	/* Every operation is store inside an object used as a collection of operands, so
+	that when the user gives an expression it will be evaluated according to the symbol */
+	const operators = {
+		"+": add,
+		"-": subtract,
+		"*": multiply,
+		"/": divide
+	}
+
+	// Perform the calculation based on the given symbol
+	if (operator in operators) {
+		return operators[operator](firstNumber, secondNumber);
+	}
+}
+
+console.log(operate("+",10,10));
