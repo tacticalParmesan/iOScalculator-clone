7f33e36644d2f3d60f903fe63e8ed9ccc36f712f
tacticalParmesan <edoardoferreri10@gmail.com>
2024-02-08 17:50:59 +0100

    Implemented reset function

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // ----------- Global variables & References -----------
 const displayValue = document.querySelector("#displayed-numbers");
-const numberButtons = document.querySelectorAll(".number-button");
-const operationButtons = document.querySelectorAll(".operation-button");
-const evaluateButton = document.querySelector("#evaluate");
-let selectedOperation = "";
+const resetButton = document.querySelector("#clear-btn");
+
+let selectedOperation = undefined;
 let firstNumber = 0;
-let operation = "";
 let secondNumber = undefined;
+let operation = "";
 let result = 0;
 let isCalculating = false;
 let canOverwrite = false;
@@ -61,12 +60,10 @@ function updateDisplayedValue(clickedNumber) {
 	that the calculator has started now or has been reset. */
 	if (displayValue.textContent == 0) {
 		displayValue.textContent = clickedNumber;
-	}
-	else if (canOverwrite) {
+	} else if (canOverwrite) {
 		displayValue.textContent = clickedNumber;
 		canOverwrite = false;
-	}
-	else {
+	} else {
 		displayValue.textContent += clickedNumber;
 	}
 }
@@ -79,34 +76,57 @@ function setCurrentOperation(clickedOperation) {
 }
 
 function listenForOperationSelection() {
-	operationButtons.forEach( function(operationButton) {
+	const operationButtons = document.querySelectorAll(".operation-button");
+
+	operationButtons.forEach(function (operationButton) {
 		operationButton.addEventListener("mousedown", (ev) => {
 			const clickedOperation = ev.target.id;
 			setCurrentOperation(clickedOperation);
-		})
-	})
+		});
+	});
 }
 
 function listenForNumberInput() {
-	numberButtons.forEach( function(numberButton) {
+	const numberButtons = document.querySelectorAll(".number-button");
+
+	numberButtons.forEach(function (numberButton) {
 		numberButton.addEventListener("mousedown", (ev) => {
 			const clickedNumberValue = ev.target.textContent;
 			updateDisplayedValue(clickedNumberValue);
-		})
-	})
+			resetButton.textContent = "C";
+		});
+	});
+}
+
+function listenForReset() {
+	resetButton.addEventListener("mousedown", resetCalculator);
 }
 
 function performOperation() {
+	const evaluateButton = document.querySelector("#evaluate");
+
 	evaluateButton.addEventListener("mousedown", () => {
-		if(isCalculating) {
+		if (selectedOperation !== undefined) {
 			secondNumber = displayValue.textContent;
 			result = operate(selectedOperation, firstNumber, secondNumber);
 			displayValue.textContent = result;
+			isCalculating = false;
+			canOverwrite = true;
 		}
-		isCalculating = false;
-		canOverwrite = true;
+	});
+}
 
-	})
+function resetCalculator() {
+	if (displayValue.textContent != 0 && !isCalculating) {
+		displayValue.textContent = 0;
+		resetButton.textContent = "AC";
+	} else if (displayValue.textContent != 0 && isCalculating) {
+		displayValue.textContent = 0;
+	} else if (displayValue.textContent == 0 && isCalculating){
+		isCalculating = false;
+		resetButton.textContent = "AC";
+		selectedOperation = undefined;
+	}
 }
 
 // ---------- Load event listeneres on page load ----------
@@ -115,8 +135,9 @@ function loadFunctions() {
 	document.addEventListener("DOMContentLoaded", () => {
 		listenForNumberInput();
 		listenForOperationSelection();
+		listenForReset();
 		performOperation();
-	})
+	});
 }
 
 loadFunctions();
